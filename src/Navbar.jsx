4fa8027a34907d9cc415dc57ca11443a81112dda
Laio <laiorbrito@gmail.com>
2026-07-04 10:24:18 -0300

    Adiciona menu hamburguer responsivo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+
 function Navbar() {
+  const [menuAberto, setMenuAberto] = useState(false)
+
   return (
     <nav className="navbar">
       <img src="/Lila_Logo_SemFundo.png" alt="Lila Bowls" className="nav-logo" />
-      <ul className="nav-links">
-        <li><a href="#sobre">Nossa História</a></li>
-        <li><a href="#cardapio">Cardápio</a></li>
-        <li><a href="#eventos">Eventos</a></li>
-        <li><a href="#ebook">Ebook</a></li>
-        <li><a href="#contato">Contato</a></li>
+      
+      <ul className={`nav-links ${menuAberto ? 'aberto' : ''}`}>
+        <li><a href="#sobre" onClick={() => setMenuAberto(false)}>Nossa História</a></li>
+        <li><a href="#cardapio" onClick={() => setMenuAberto(false)}>Cardápio</a></li>
+        <li><a href="#eventos" onClick={() => setMenuAberto(false)}>Eventos</a></li>
+        <li><a href="#ebook" onClick={() => setMenuAberto(false)}>Ebook</a></li>
+        <li><a href="#contato" onClick={() => setMenuAberto(false)}>Contato</a></li>
       </ul>
+
+      <button 
+        className="menu-hamburguer"
+        onClick={() => setMenuAberto(!menuAberto)}
+      >
+        {menuAberto ? '✕' : '☰'}
+      </button>
     </nav>
   )
 }
